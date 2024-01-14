643ce3d433d232c35073bb3f1211443bc6c2d4c1
tomat <xxlennygamerxx@gmail.com>
2024-01-13 23:05:26 -0800

    Move diffing projects.

---
+++ src/Tomat.Differ/Identity/Steam/Manifest.cs
@@ -0,0 +1,16 @@
+﻿namespace Tomat.Differ.Identity.Steam;
+
+/// <summary>
+///     An immutable manifest identifier for a depot.
+/// </summary>
+public readonly struct Manifest {
+    public int DepotId { get; }
+
+    // Consistency for us...
+    public string Name { get; }
+
+    public Manifest(int depotId, string name) {
+        DepotId = depotId;
+        Name = name;
+    }
+}
